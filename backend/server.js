6b2d2a9fef718688fc50615a6895a030ccecdd3e
Tuba_Khan <121151121+tubakhan0415@users.noreply.github.com>
2024-05-31 15:01:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -268,5 +268,5 @@ app.post('/change-password', authMiddleware, async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on :${port}`);
 });
